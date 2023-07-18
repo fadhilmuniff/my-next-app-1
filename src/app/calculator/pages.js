3c5af6d4535b042358939f1bd1a1145d0cdfda91
fadhilmuniff <fadil123wew@gmail.com>
2023-07-18 22:21:57 +0700

    new app

---
+++ src/app/calculator/pages.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+// import { useEffect } from 'react';
+
+export default function Calculator() {
+    const angka1 = useEffect(0)
+    return (
+        <div>
+            <p>ini Calculator</p>
+        </div>
+    );
+}
