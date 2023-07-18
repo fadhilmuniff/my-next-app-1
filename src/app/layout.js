780782a1244d48c5e7a7aafab81ab96a5e43f049
fadhilmuniff <fadil123wew@gmail.com>
2023-07-18 22:22:12 +0700

    v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head />
       <body className={inter.className}>
         <ul>
           <li><Link href='/'>home</Link></li>
